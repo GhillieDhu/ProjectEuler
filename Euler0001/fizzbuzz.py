806f691e96799689afec35c2a590138fbf4b2f4d
Mark Phariss <GhillieDhu@users.noreply.github.com>
2018-03-11 04:08:41 +0000

    typing

--- Euler0001/fizzbuzz.py
+++ Euler0001/fizzbuzz.py
@@ -1,15 +1,16 @@
 from functools import partial
-from typing import List, Callable
+from typing import Callable, Set, Sequence
+import typing
 
 
-def filtered_range(pred: Callable[[int], bool], cap: int) -> List[int]:
+def filtered_range(pred: Callable[[int], bool], cap: int) -> Set[int]:
     """"""
-    return [i for i in range(1, cap) if pred(i)]
+    return {i for i in range(1, cap) if pred(i)}
 
 
-def multiple_of_any(multiplicands, i):
-    return any([i % m == 0 for m in multiplicands])
+def multiple_of_any(multiplicands: Sequence[int], i: int):
+    return any({i % m == 0 for m in multiplicands})
 
 
-def sum_of_multiples(multiplicands, cap):
+def sum_of_multiples(multiplicands: Sequence[int], cap: int):
     return sum(filtered_range(partial(multiple_of_any, multiplicands), cap))
