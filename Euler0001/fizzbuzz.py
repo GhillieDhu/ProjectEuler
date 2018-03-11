7c645dcee339cc8cfa7bd7457354704495273ee9
Mark Phariss <GhillieDhu@users.noreply.github.com>
2018-03-11 03:54:44 +0000

    mypy & pep8 linting

--- Euler0001/fizzbuzz.py
+++ Euler0001/fizzbuzz.py
@@ -1,10 +1,15 @@
-import functools
+from functools import partial
+from typing import List, Callable
 
-def filtered_range(pred, cap):
+
+def filtered_range(pred: Callable[[int], bool], cap: int) -> List[int]:
+    """"""
     return [i for i in range(1, cap) if pred(i)]
+
 
 def multiple_of_any(multiplicands, i):
     return any([i % m == 0 for m in multiplicands])
 
+
 def sum_of_multiples(multiplicands, cap):
-    return sum(filtered_range(functools.partial(multiple_of_any, multiplicands), cap))
+    return sum(filtered_range(partial(multiple_of_any, multiplicands), cap))
--- Euler0001/fizzbuzz_properties.py
+++ Euler0001/fizzbuzz_properties.py
@@ -1,3 +1,4 @@
 import fizzbuzz
+import hypothesis
 
 fizzbuzz.sum_of_multiples([3, 5], 10) == 23
