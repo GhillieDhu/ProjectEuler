2f457aa85eb9c5732980f1684091211aff2ff15e
Mark Phariss <GhillieDhu@users.noreply.github.com>
2018-03-13 01:25:09 +0000

    Problem #2 updated

--- Euler0002/fibonacci_test.py
+++ Euler0002/fibonacci_test.py
@@ -3,6 +3,6 @@ from hypothesis import given
 import hypothesis.strategies
 
 
-@given(hypothesis.strategies.integers())
-def test_dummy(x):
-    assert False
+def test_example():
+    assert list(fibonacci.firstN(1, 2, 10)) == \
+           [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
