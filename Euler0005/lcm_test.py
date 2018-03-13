5dd4acceb07c910dbad501f456c8528ef6f70fcb
Mark Phariss <GhillieDhu@users.noreply.github.com>
2018-03-13 15:53:39 -0700

    Property tests for #5; not robust against [] or [1]

--- Euler0005/lcm_test.py
+++ Euler0005/lcm_test.py
@@ -1,6 +1,24 @@
 from .lcm import lcm
 from typing import Sequence, List
+from hypothesis import given
+from hypothesis.strategies import integers, lists
 
 
 def test_example():
     assert lcm(range(1, 11)) == 2520
+
+
+@given(integers(2))
+def test_single(x: int):
+    assert lcm([x]) == x
+
+
+@given(integers(2), integers(1, 5))
+def test_duplication_irrelevant(x: int, n: int):
+    assert lcm([x] * n) == x
+
+
+@given(lists(integers(2), min_size=2, max_size=10))
+def test_divisibility(xs: List[int]):
+    product = lcm(xs)
+    assert all(product % x == 0 for x in xs)
