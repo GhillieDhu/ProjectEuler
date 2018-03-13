588e8c36d77b4419f4448f79e158d8a4e334e4f3
Mark Phariss <GhillieDhu@users.noreply.github.com>
2018-03-13 10:35:02 -0700

    Property tests for #4

--- Euler0004/palindrome_test.py
+++ Euler0004/palindrome_test.py
@@ -1,5 +1,16 @@
 from palindrome import max_palindrome, is_palindrome
+from hypothesis import given
+from hypothesis.strategies import characters
 
 
 def test_example():
     assert max_palindrome(2) == 9009
+
+
+def test_empty_string():
+    assert is_palindrome('')
+
+
+@given(characters())
+def test_single_character(c):
+    assert is_palindrome(c)
