cd2b55ef29711fc228c8ad8dd6d4933ff2adf6f2
Mark Phariss <mapharis@microsoft.com>
2018-03-14 19:27:57 -0700

    2-3x speedup by switching set to outer

--- Euler0010/primesbelow.py
+++ Euler0010/primesbelow.py
@@ -1,11 +1,11 @@
 from functools import reduce
+from operator import add
 from typing import Set
 
 
 def primes_to(n: int) -> Set[int]:
     xs = range(n, 1, -1)
-    cs: Set = reduce(lambda a, b: a | b,
-                     [set(range(x**2, n + 1, x)) for x in xs])
+    cs: Set = set(reduce(add, [list(range(x**2, n + 1, x)) for x in xs]))
     return set(xs) - cs
 
 
